4bdbf2c881a8884c48b3fab234d9592d4cb7eb73
Michael Bylstra <mbylstra@gmail.com>
2025-10-06 13:59:37 +1100

    Refactor theme configuration to use a custom ColorScheme for improved styling

--- lib/main.dart
+++ lib/main.dart
@@ -14,22 +14,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: const ColorScheme(
+          brightness: Brightness.light,
+          primary: Color(0xFF1A1A1A),        // Dark grey
+          onPrimary: Color(0xFFF5F5F5),      // Off-white
+          secondary: Color(0xFF4A4A4A),      // Medium grey
+          onSecondary: Color(0xFFF5F5F5),    // Off-white
+          error: Color(0xFF333333),          // Dark grey for errors
+          onError: Color(0xFFFFFFFF),        // White
+          surface: Color(0xFFFAFAFA),        // Very light grey
+          onSurface: Color(0xFF1A1A1A),      // Dark grey
+        ),
         textTheme: GoogleFonts.workSansTextTheme(),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page 2'),
