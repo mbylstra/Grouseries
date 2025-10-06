4e410a20a44a3cd5fabf1e5baf1e983f1a290f84
Michael Bylstra <mbylstra@gmail.com>
2025-10-06 13:55:45 +1100

    Add Google Fonts dependency and update main.dart to use Work Sans theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,6 +30,7 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: GoogleFonts.workSansTextTheme(),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page 2'),
     );
